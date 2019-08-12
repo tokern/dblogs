ac36529869385b8ca268d3a21d796cbcc26aac39
Rajat Venkatesh <vrajat@users.noreply.github.com>
2019-08-12 10:35:42 +0530

    fix:dev:Clean up older versions of docker, k8s and test files

--- docker/logrotate/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Docker Entrypoint, generates logrotate config file, sets up crontab and hand over to `tini`
-
-echo "$CRON_SCHEDULE /usr/sbin/logrotate /etc/logrotate/logrotate.conf" | crontab -
-
-exec tini $@
